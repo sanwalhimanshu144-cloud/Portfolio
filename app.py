d98e4b2d98a98e2dda0d4caaa8f1e9275a91894c
sanwalhimanshu144-cloud <sanwalhimanshu144@gmail.com>
2026-05-12 23:38:31 +0530

    Updated portfolio code

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("My AI/ML Projects")
 
-st.subheader("2. CineMatch-AI")
+st.subheader("1. CineMatch-AI")
 st.write("Movie recommendation system.")
 st.link_button("View App", "https://cinematch-ai.streamlit.app")
